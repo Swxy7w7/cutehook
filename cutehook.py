597509dabcca3dd2534964a3a24f8b41db21b455
Swxy <72712659+Swxy7w7@users.noreply.github.com>
2022-11-08 06:42:54 -0500

    Update cutehook.py

--- cutehook.py
+++ cutehook.py
@@ -1,54 +1,92 @@
-import requests, colorama, time, os
+import os
+import requests
+from requests import get, post
+from time import sleep 
+from sys import stdout as _stdout
+from os import name as _name
+from ctypes import c_int, c_byte, Structure, byref, windll
 
+def _colors(color: str) -> str:
+    return f"\033[38;2;{color}m"
+
+c = _colors('0;255;196')
+m = _colors('155;0;72')
+g = _colors('0;255;0')
+r = _colors('255;0;0')
+w = _colors('255;255;255')
+rl = _colors('255;28;59')
+ml = _colors('255;0;102')
+gl = _colors('0;255;128')
+
+if _name == 'nt':
+    class _OcultInfo(Structure):
+        _fields_ = [("size", c_int),
+                    ("visible", c_byte)]
+
+class Ocult:
+
+    def Ocultism():
+        if _name == 'nt':
+            Ocult._ocultism(False)
+        elif _name == 'posix':
+            _stdout.write("\033[?25l")
+            _stdout.flush()
+
+    def _ocultism(visible: bool):
+        ci = _OcultInfo()
+        handle = windll.kernel32.GetStdHandle(-11)
+        windll.kernel32.GetConsoleCursorInfo(handle, byref(ci))
+        ci.visible = visible
+        windll.kernel32.SetConsoleCursorInfo(handle, byref(ci))
 
 def _exit():
-    time.sleep(5)
+    sleep(5)
     exit()
 
 
 def check_hook(hook):
-    info = requests.get(hook).text
+    info = get(hook).text
     if "\"message\": \"Unknown Webhook\"" in info:
         return False
     return True
-
 
 def main(webhook, name, delay, amount, message, hookDeleter):
     counter = 0
     while True if amount == "inf" else counter < int(amount):
         try:
-            data = requests.post(webhook, json={"content": str(message), "name": str(name), "avatar_url": "https://i.imgur.com/lk79Hlc.jpeg"})
-            if data.status_code == 204:
-                print(f"{colorama.Back.MAGENTA} {colorama.Fore.WHITE}[+] Sent{colorama.Back.RESET}")
+            payload = post(webhook, json={"content": str(message), "name": str(name), "avatar_url": "https://i.imgur.com/lk79Hlc.jpeg"})
+            if payload.status_code == 204:
+                print(f" {m}[{w}+{m}] Sent")
             else:
-                print(f"{colorama.Back.RED} {colorama.Fore.WHITE}[-] Fail{colorama.Back.RESET}")
+                print(f" {r}[-] Fail")
         except:
             print()
-        time.sleep(float(delay))
+        sleep(float(delay))
         counter += 1
     if hookDeleter.lower() == "y":
         requests.delete(webhook)
-        print(f'{colorama.Fore.MAGENTA}webhook deleted')
-    print(f'{colorama.Fore.GREEN}done...')
+        print(f'{g}webhook deleted')
+    print(f'{g}done...')
 
 
-def initialize():
-    print(f"""{colorama.Fore.MAGENTA}
-                   __           __                      __         
-                  /\ \__       /\ \                    /\ \        
-      ___   __  __\ \ ,_\    __\ \ \___     ___     ___\ \ \/'\    
-     /'___\/\ \/\ \\\\ \ \/  /'__`\ \  _ `\  / __`\  / __`\ \ , <    
-    /\ \__/\ \ \_\ \\\\ \ \_/\  __/\ \ \ \ \/\ \L\ \/\ \L\ \ \ \\\\`\  
-    \ \____\\\\ \____/ \ \__\ \____\\\\ \_\ \_\ \____/\ \____/\ \_\ \_\\
-     \/____/ \/___/   \/__/\/____/ \/_/\/_/\/___/  \/___/  \/_/\/_/
-                                                          by cattyn
+def CHook():
+    Ocult.Ocultism()
+    print(f"""
+    {ml}               __          {rl} __                      __         
+    {ml}              /\ \__       {rl}/\ \                    /\ \        
+    {ml}  ___   __  __\ \ ,_\    __{rl}\ \ \___     ___     ___\ \ \/'\    
+    {ml} /'___\/\ \/\ \\\\ \ \/  /'__`{rl}\ \  _ `\  / __`\  / __`\ \ , <    
+    {ml}/\ \__/\ \ \_\ \\\\ \ \_/\  __/{rl}\ \ \ \ \/\ \L\ \/\ \L\ \ \ \\\\`\  
+    {ml}\ \____\\\\ \____/ \ \__\ \____{rl}\\\\ \_\ \_\ \____/\ \____/\ \_\ \_\\
+     {ml}\/____/ \/___/   \/__/\/____/ {rl}\/_/\/_/\/___/  \/___/  \/_/\/_/
+                                                          {gl}by cattyn
      """)
-    webhook = input("Enter ur webhook > ")
-    name = input("Enter a webhook name > ")
-    message = input("Enter a message > ")
-    delay = input("Enter a delay [int/float] > ")
-    amount = input("Enter an amount [int/inf] > ")
-    hookDeleter = input("Delete webhook after spam? [Y/N] > ")
+    webhook = input(f" {ml}Enter ur webhook {c}> {w}")
+    name = input(f" {ml}Enter a webhook name {c}> {w}")
+    message = input(f" {ml}Enter a message {c}> {w}")
+    delay = input(f" {ml}Enter a delay {r}[int/float] {c}> {w}")
+    amount = input(f" {ml}Enter an amount {r}[int/inf] {c}> {w}")
+    hookDeleter = input(f" {ml}Delete webhook after spam? {r}[Y/N] {c}> {w}")
     try:
         delay = float(delay)
     except ValueError:
@@ -61,7 +99,7 @@ def initialize():
 
 
 if __name__ == '__main__':
-    os.system('cls')
+    Windows = _name == 'nt'
+    os.system('cls' if Windows else 'clear')
     os.system('title cutehook on top LOL')
-    colorama.init()
-    initialize()
+    CHook()
